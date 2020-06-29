04d07569f52f4ea1aa613b590b80cdbf46f14f71
hizukayudai@gmail.com <09120912Abcd>
2020-06-30 03:48:09 +0900

    add/UI

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
+    <!-- header -->
     <v-app-bar
       color="deep-purple accent-4"
       dense
-      dark
+      app
+      style="height: 70px;"
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
       <v-toolbar-title>Page title</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
       <v-menu
         left
         bottom
@@ -44,63 +38,39 @@
         </v-list>
       </v-menu>
     </v-app-bar>
-    <Nuxt />
+    <!-- content -->
+    <div class="container">
+      <Nuxt />
+    </div>
+    <!-- footer -->
+    <v-bottom-navigation
+      v-model="bottomNav"
+      app
+      style="height: 70px;"
+    >
+      <v-btn value="recent">
+        <span>Recent</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+<style scoped>
+.container {
+  padding: 80px 10px 10px;
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 70px);
 }
 </style>
