992b8f22b88f0ec88b79bd397a07ffa2529651c3
hizukayudai@gmail.com <09120912Abcd>
2020-07-23 04:03:50 +0900

    fix

--- app/components/molecules/common/AppHeader.vue
+++ app/components/molecules/common/AppHeader.vue
@@ -32,27 +32,32 @@
       temporary
     >
       <v-list-item style="height: 70px;">
-        <v-list-item-avatar>
+        <v-list-item-avatar style="margin-right: 10px;">
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg"></v-img>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title></v-list-item-title>
+          <v-list-item-title>Travis Howard</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list dense>
-
+      <v-list
+        dense
+        nav
+      >
         <v-list-item
+          v-for="item in items"
+          :key="item.title"
           link
         >
-          <v-list-item-icon>
-            <v-icon></v-icon>
+          <v-list-item-icon style="margin-right: 10px;">
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title></v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -65,6 +70,11 @@
     data () {
       return {
         drawer: null,
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
       }
     },
   }
